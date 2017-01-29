f4c74fe930d679dc25875fa47fe9759fd2aaaca9
Fernanda <fernandamora88@gmail.com>
2017-01-29 12:24:18 -0800

    add my_bsearc

--- a01-prep-todd/lib/searches_and_sorts.rb
+++ a01-prep-todd/lib/searches_and_sorts.rb
@@ -60,8 +60,36 @@ class Array
   end
 
   def my_quick_sort(&prc)
+    #select pivot as the first one
+    #take all the elements that are smaller than pivot to the left
+    #take the big ones to the right
+    #sort each one of them
+    #return when your array is one or less
+    return self if self.length <= 1
+
+    prc ||= Proc.new { |x,y| x <=> y}
+
+    pivot = self.first
+    left = self.drop(1).select {|n| prc.call(pivot,n) == 1}
+    right = self.drop(1).select { |n| prc.call(pivot,n) != 1}
+
+    left.my_quick_sort(&prc) + [pivot] + right.my_quick_sort(&prc)
   end
 
   def my_bsearch(target, &prc)
+    return nil if self.empty?
+    prc ||= Proc.new {|x,y| x <=> y}
+
+    middle = self.length/2
+    return middle if prc.call(self[middle],target) == 0
+
+    if prc.call(self[middle],target) == 1
+      result = self.take(middle).my_bsearch(target, &prc)
+      return result unless result.nil?
+    else
+      result = self.drop(middle + 1).my_bsearch(target, &prc)
+      return result + 1 + middle unless result.nil?
+    end
+    nil
   end
 end
