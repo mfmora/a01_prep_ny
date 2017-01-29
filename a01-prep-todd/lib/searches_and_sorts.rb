62cba1ac17ddde6b5ae063a6ad9f14ef53bff6ce
Fernanda <fernandamora88@gmail.com>
2017-01-29 11:55:30 -0800

    add bubble sort

--- a01-prep-todd/lib/searches_and_sorts.rb
+++ a01-prep-todd/lib/searches_and_sorts.rb
@@ -1,15 +1,43 @@
 #### Searches && Sorts ####
-
+require 'byebug'
 class Array
   def bubble_sort!
+    sorted = false
+    until sorted
+      sorted = true
+      each_index do |i|
+        next if i + 1 == self.length
+        j = i + 1
+        if self[i] > self[j]
+          self[i],self[j] = self[j],self[i]
+          sorted = false
+        end
+      end
+    end
+    self
   end
 
   def bubble_sort!(&prc)
+    prc ||= Proc.new { |x,y| x <=> y}
+    sorted = false
+    until sorted
+      sorted = true
+      each_index do |i|
+        next if i + 1 == self.length
+        j = i + 1
+        if prc.call(self[i],self[j]) == 1
+          self[i],self[j] = self[j],self[i]
+          sorted = false
+        end
+      end
+    end
+    self
   end
 
   def bubble_sort(&prc)
+    self.dup.bubble_sort!(&prc)
   end
-  
+
   def merge_sort(&prc)
   end
 
